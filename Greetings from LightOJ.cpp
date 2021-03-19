efe0644d45708773ca3652878b2d43bbec86340a
sbswarna <79458082+sbswarna@users.noreply.github.com>
2021-03-19 09:28:21 +0600

    LOJ-1000

---
+++ Greetings from LightOJ.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int tst,a,b;
+int main()
+{
+    cin>>tst;
+    int k=1;
+    while(tst--)
+    {
+        cin>>a>>b;
+        printf("Case %d: %d\n", k++, a+b);
+    }
+    return 0;
+}
